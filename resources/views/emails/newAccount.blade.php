a9871e2cee384fbe3d0f7457b03896e4f891e541
byamungulewis01 <ntwarilebon09@gmail.com>
2024-03-15 18:29:31 +0200

    email template

--- resources/views/emails/newAccount.blade.php
+++ resources/views/emails/newAccount.blade.php
@@ -1,9 +1,10 @@
 @component('mail::message')
-# Welcome to {{ config('app.name') }}
+    # Welcome to {{ config('app.name') }}
 
-Your new NZIZA MIS account have been created, credential are:
+    Your new NZIZA MIS account have been created, credential are:
 
-Email: {{ $email }} <br>
-Password: {{ $password }}
-
+    Email: {{ $email }} <br>
+    Password: {{ $password }}
+    <br>
+    Click here to login <a href="https://nzizamis.com/login" target="_blank">Employee Operations Management System</a>
 @endcomponent
